d102f84595b8519e89b74e89be30cc6b418d7e7a
Kim Như Vũ <59812699+kimnhuvu@users.noreply.github.com>
2020-12-14 09:51:44 +0700

    Như push giao diện

---
+++ QuanLyKho/DangNhap.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace QuanLyKho
+{
+     partial class DangNhap
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.lb_tbao = new System.Windows.Forms.Label();
+               this.btn_Exit = new System.Windows.Forms.Button();
+               this.btn_Dangnhap = new System.Windows.Forms.Button();
+               this.txt_Pass_dangnhap = new System.Windows.Forms.TextBox();
+               this.txt_SDT_dangnhap = new System.Windows.Forms.TextBox();
+               this.label5 = new System.Windows.Forms.Label();
+               this.label4 = new System.Windows.Forms.Label();
+               this.label3 = new System.Windows.Forms.Label();
+               this.label2 = new System.Windows.Forms.Label();
+               this.label1 = new System.Windows.Forms.Label();
+               this.SuspendLayout();
+               // 
+               // lb_tbao
+               // 
+               this.lb_tbao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.lb_tbao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+               this.lb_tbao.Location = new System.Drawing.Point(-2, 178);
+               this.lb_tbao.Name = "lb_tbao";
+               this.lb_tbao.Size = new System.Drawing.Size(410, 35);
+               this.lb_tbao.TabIndex = 19;
+               this.lb_tbao.Text = "*Thông tin đăng nhập không đúng!";
+               this.lb_tbao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+               this.lb_tbao.Visible = false;
+               // 
+               // btn_Exit
+               // 
+               this.btn_Exit.FlatAppearance.BorderSize = 0;
+               this.btn_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+               this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.btn_Exit.ForeColor = System.Drawing.Color.White;
+               this.btn_Exit.Location = new System.Drawing.Point(362, 3);
+               this.btn_Exit.Name = "btn_Exit";
+               this.btn_Exit.Size = new System.Drawing.Size(49, 43);
+               this.btn_Exit.TabIndex = 18;
+               this.btn_Exit.Text = "X";
+               this.btn_Exit.UseVisualStyleBackColor = true;
+               // 
+               // btn_Dangnhap
+               // 
+               this.btn_Dangnhap.BackColor = System.Drawing.Color.DodgerBlue;
+               this.btn_Dangnhap.FlatAppearance.BorderSize = 0;
+               this.btn_Dangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+               this.btn_Dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.btn_Dangnhap.ForeColor = System.Drawing.Color.White;
+               this.btn_Dangnhap.Location = new System.Drawing.Point(130, 223);
+               this.btn_Dangnhap.Name = "btn_Dangnhap";
+               this.btn_Dangnhap.Size = new System.Drawing.Size(138, 35);
+               this.btn_Dangnhap.TabIndex = 17;
+               this.btn_Dangnhap.Text = "Đăng nhập";
+               this.btn_Dangnhap.UseVisualStyleBackColor = false;
+               // 
+               // txt_Pass_dangnhap
+               // 
+               this.txt_Pass_dangnhap.BackColor = System.Drawing.Color.Teal;
+               this.txt_Pass_dangnhap.BorderStyle = System.Windows.Forms.BorderStyle.None;
+               this.txt_Pass_dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.txt_Pass_dangnhap.ForeColor = System.Drawing.Color.White;
+               this.txt_Pass_dangnhap.Location = new System.Drawing.Point(107, 145);
+               this.txt_Pass_dangnhap.Multiline = true;
+               this.txt_Pass_dangnhap.Name = "txt_Pass_dangnhap";
+               this.txt_Pass_dangnhap.PasswordChar = '*';
+               this.txt_Pass_dangnhap.Size = new System.Drawing.Size(199, 23);
+               this.txt_Pass_dangnhap.TabIndex = 16;
+               // 
+               // txt_SDT_dangnhap
+               // 
+               this.txt_SDT_dangnhap.BackColor = System.Drawing.Color.Teal;
+               this.txt_SDT_dangnhap.BorderStyle = System.Windows.Forms.BorderStyle.None;
+               this.txt_SDT_dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.txt_SDT_dangnhap.ForeColor = System.Drawing.Color.White;
+               this.txt_SDT_dangnhap.Location = new System.Drawing.Point(107, 88);
+               this.txt_SDT_dangnhap.Multiline = true;
+               this.txt_SDT_dangnhap.Name = "txt_SDT_dangnhap";
+               this.txt_SDT_dangnhap.Size = new System.Drawing.Size(199, 23);
+               this.txt_SDT_dangnhap.TabIndex = 15;
+               // 
+               // label5
+               // 
+               this.label5.BackColor = System.Drawing.Color.White;
+               this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label5.ForeColor = System.Drawing.Color.White;
+               this.label5.Location = new System.Drawing.Point(107, 170);
+               this.label5.Name = "label5";
+               this.label5.Size = new System.Drawing.Size(200, 1);
+               this.label5.TabIndex = 14;
+               // 
+               // label4
+               // 
+               this.label4.BackColor = System.Drawing.Color.White;
+               this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label4.ForeColor = System.Drawing.Color.White;
+               this.label4.Location = new System.Drawing.Point(107, 113);
+               this.label4.Name = "label4";
+               this.label4.Size = new System.Drawing.Size(200, 1);
+               this.label4.TabIndex = 13;
+               // 
+               // label3
+               // 
+               this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label3.ForeColor = System.Drawing.Color.White;
+               this.label3.Location = new System.Drawing.Point(26, 148);
+               this.label3.Name = "label3";
+               this.label3.Size = new System.Drawing.Size(80, 23);
+               this.label3.TabIndex = 12;
+               this.label3.Text = "PASS:";
+               this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+               // 
+               // label2
+               // 
+               this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label2.ForeColor = System.Drawing.Color.White;
+               this.label2.Location = new System.Drawing.Point(40, 91);
+               this.label2.Name = "label2";
+               this.label2.Size = new System.Drawing.Size(66, 23);
+               this.label2.TabIndex = 11;
+               this.label2.Text = "SĐT:";
+               this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+               // 
+               // label1
+               // 
+               this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+               this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+               this.label1.ForeColor = System.Drawing.Color.White;
+               this.label1.Location = new System.Drawing.Point(0, 0);
+               this.label1.Name = "label1";
+               this.label1.Size = new System.Drawing.Size(408, 62);
+               this.label1.TabIndex = 10;
+               this.label1.Text = "ĐĂNG NHẬP";
+               this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+               // 
+               // DangNhap
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.BackColor = System.Drawing.Color.Teal;
+               this.ClientSize = new System.Drawing.Size(408, 261);
+               this.Controls.Add(this.lb_tbao);
+               this.Controls.Add(this.btn_Exit);
+               this.Controls.Add(this.btn_Dangnhap);
+               this.Controls.Add(this.txt_Pass_dangnhap);
+               this.Controls.Add(this.txt_SDT_dangnhap);
+               this.Controls.Add(this.label5);
+               this.Controls.Add(this.label4);
+               this.Controls.Add(this.label3);
+               this.Controls.Add(this.label2);
+               this.Controls.Add(this.label1);
+               this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+               this.Name = "DangNhap";
+               this.Text = "DangNhap";
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.Label lb_tbao;
+          private System.Windows.Forms.Button btn_Exit;
+          private System.Windows.Forms.Button btn_Dangnhap;
+          private System.Windows.Forms.TextBox txt_Pass_dangnhap;
+          private System.Windows.Forms.TextBox txt_SDT_dangnhap;
+          private System.Windows.Forms.Label label5;
+          private System.Windows.Forms.Label label4;
+          private System.Windows.Forms.Label label3;
+          private System.Windows.Forms.Label label2;
+          private System.Windows.Forms.Label label1;
+     }
+}
